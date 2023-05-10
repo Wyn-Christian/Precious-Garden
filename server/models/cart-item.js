34e4f788e8d1650f4594bec5c3a1b9d4d65dc518
Wyn Chrsitian <wynchristian.rebanal@tup.edu.ph>
2023-05-10 16:26:36 +0800

    Create Cart Item Model

---
+++ server/models/cart-item.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CartItemSchema = new Schema(
+  {
+    customer: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    product: {
+      type: mongoose.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      default: 1,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Cart-Item", CartItemSchema);
