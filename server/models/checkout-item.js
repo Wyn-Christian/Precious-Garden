8eb804c2e5ed751888fd41ed3957ca701002d057
Wyn Chrsitian <wynchristian.rebanal@tup.edu.ph>
2023-05-18 10:02:30 +0800

    create checkout-item model

---
+++ server/models/checkout-item.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CheckoutItemSchema = new Schema(
+  {
+    customer: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    product: {
+      type: mongoose.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    price: {
+      type: mongoose.Types.Decimal128,
+      get: getValue,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+function getValue(value) {
+  if (typeof value !== "undefined") {
+    return parseFloat(value.toString());
+  }
+  return value;
+}
+
+module.exports = mongoose.model("Checkout-Item", CheckoutItemSchema);
