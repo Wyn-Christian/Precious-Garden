07e226260f864f00267c0abef6352d937d76faab
Wyn Chrsitian <wynchristian.rebanal@tup.edu.ph>
2023-05-18 10:02:46 +0800

    create checkout model

--- server/models/checkout.js
+++ server/models/checkout.js
@@ -1,27 +1,69 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const CheckoutSchema = new Schema({
-  customer: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  checkout_items: [
-    {
+const CheckoutSchema = new Schema(
+  {
+    customer: {
       type: mongoose.Types.ObjectId,
-      ref: "Checkout-Item",
+      ref: "User",
       required: true,
     },
-  ],
-  total_quantiy: {
-    type: Number,
-    required: true,
+    checkout_items: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Checkout-Item",
+        required: true,
+      },
+    ],
+    total_quantiy: {
+      type: Number,
+      required: true,
+    },
+    total_price: {
+      type: mongoose.Types.Decimal128,
+      get: getValue,
+      required: true,
+    },
   },
-  total_price: {
-    type: mongoose.Types.Decimal128,
-    required: true,
+  {
+    timestamps: true,
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+function getValue(value) {
+  if (typeof value !== "undefined") {
+    return parseFloat(value.toString());
+  }
+  return value;
+}
+
+const CheckoutSaleSchema = new Schema(
+  {
+    sale_price: { type: mongoose.Types.Decimal128, required: true },
+    timestamp: { type: Date, default: Date.now },
   },
+  {
+    timeseries: {
+      timeField: "timestamp",
+    },
+  }
+);
+const CheckoutSale = mongoose.model("sale-checkout", CheckoutSaleSchema);
+
+CheckoutSchema.post("save", function (doc) {
+  let checkout_sale = new CheckoutSale({
+    sale_price: doc.total_price,
+  });
+  checkout_sale
+    .save()
+    .then((result) => {
+      console.log("Create Checkout Sale Successfully", result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = mongoose.model("Checkout", CheckoutSchema);
