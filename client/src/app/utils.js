070f60aeced462539140f47c50dd3ebd208c6de6
Wyn Chrsitian <wynchristian.rebanal@tup.edu.ph>
2023-05-28 19:32:54 +0800

    Create utils

---
+++ client/src/app/utils.js
@@ -0,0 +1,6 @@
+export const api_base_url = import.meta.env.VITE_SERVER_URI;
+export const PHPPrice = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "PHP",
+});
+export const drawerWidth = 240;
