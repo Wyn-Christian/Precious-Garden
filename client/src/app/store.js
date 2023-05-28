70123696aaa3f86f9e3d18827147c09b853fcf73
Wyn Chrsitian <wynchristian.rebanal@tup.edu.ph>
2023-05-28 19:33:09 +0800

    Create redux store

---
+++ client/src/app/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "../features/apiSlice";
+
+import userReducer from "../features/userSlice";
+
+export default configureStore({
+  reducer: {
+    user: userReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+});
