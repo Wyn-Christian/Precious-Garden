cd7b6d17622747ca3c24a5970a28fefd746eadf7
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2023-07-08 03:24:36 +0800

    Update indentations

--- client/src/components/Navbar/ClientNavbar.jsx
+++ client/src/components/Navbar/ClientNavbar.jsx
@@ -3,20 +3,20 @@ import { removeUser, userSelector } from "../../features/userSlice";
 import { Link } from "react-router-dom";
 
 import {
-  AppBar,
-  Avatar,
-  Badge,
-  Box,
-  Button,
-  Container,
-  Divider,
-  IconButton,
-  Menu,
-  MenuItem,
-  Toolbar,
-  Tooltip,
-  Typography,
-  useScrollTrigger,
+	AppBar,
+	Avatar,
+	Badge,
+	Box,
+	Button,
+	Container,
+	Divider,
+	IconButton,
+	Menu,
+	MenuItem,
+	Toolbar,
+	Tooltip,
+	Typography,
+	useScrollTrigger,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 
@@ -29,209 +29,209 @@ import { api_base_url } from "../../app/utils";
 import { enqueueSnackbar } from "notistack";
 
 const CartItemsButton = () => {
-  const [totalItems, setTotalItems] = useState(0);
-  const user = useSelector(userSelector);
-  const { data: cart_items = [] } = useGetCartByUserQuery(user.id);
+	const [totalItems, setTotalItems] = useState(0);
+	const user = useSelector(userSelector);
+	const { data: cart_items = [] } = useGetCartByUserQuery(user.id);
 
-  useEffect(() => {
-    let total_num = cart_items.reduce((total, item) => {
-      return total + item.quantity;
-    }, 0);
-    setTotalItems(total_num);
-  }, [cart_items]);
+	useEffect(() => {
+		let total_num = cart_items.reduce((total, item) => {
+			return total + item.quantity;
+		}, 0);
+		setTotalItems(total_num);
+	}, [cart_items]);
 
-  return (
-    <IconButton
-      size="large"
-      color="inherit"
-      LinkComponent={Link}
-      to="/customer/cart-items"
-    >
-      <Badge badgeContent={totalItems} color="error">
-        <ShoppingCartIcon />
-      </Badge>
-    </IconButton>
-  );
+	return (
+		<IconButton
+			size="large"
+			color="inherit"
+			LinkComponent={Link}
+			to="/customer/cart-items"
+		>
+			<Badge badgeContent={totalItems} color="error">
+				<ShoppingCartIcon />
+			</Badge>
+		</IconButton>
+	);
 };
 
 const NavItemLink = ({ to, title }) => (
-  <Box
-    component={Link}
-    to={to}
-    sx={{ textDecoration: "none", color: "inherit" }}
-  >
-    <Button sx={{ my: 2, color: "black", display: "block" }} disableRipple>
-      {title}
-    </Button>
-  </Box>
+	<Box
+		component={Link}
+		to={to}
+		sx={{ textDecoration: "none", color: "inherit" }}
+	>
+		<Button sx={{ my: 2, color: "black", display: "block" }} disableRipple>
+			{title}
+		</Button>
+	</Box>
 );
 
 const MenuNavItemLink = ({ to, title, handleCloseNavMenu }) => (
-  <Box
-    component={Link}
-    to={to}
-    sx={{ textDecoration: "none", color: "inherit" }}
-  >
-    <MenuItem onClick={handleCloseNavMenu}>
-      <Typography variant="button">{title}</Typography>
-    </MenuItem>
-  </Box>
+	<Box
+		component={Link}
+		to={to}
+		sx={{ textDecoration: "none", color: "inherit" }}
+	>
+		<MenuItem onClick={handleCloseNavMenu}>
+			<Typography variant="button">{title}</Typography>
+		</MenuItem>
+	</Box>
 );
 
 const UserAvatarMenu = () => {
-  const dispatch = useDispatch();
-  const user = useSelector(userSelector);
+	const dispatch = useDispatch();
+	const user = useSelector(userSelector);
 
-  const [anchorElUser, setAnchorElUser] = useState(null);
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-  const handleLogout = () => {
-    dispatch(removeUser());
-    enqueueSnackbar("Log out Successfully", { variant: "success" });
-    setAnchorElUser(null);
-  };
+	const [anchorElUser, setAnchorElUser] = useState(null);
+	const handleOpenUserMenu = (event) => {
+		setAnchorElUser(event.currentTarget);
+	};
+	const handleCloseUserMenu = () => {
+		setAnchorElUser(null);
+	};
+	const handleLogout = () => {
+		dispatch(removeUser());
+		enqueueSnackbar("Log out Successfully", { variant: "success" });
+		setAnchorElUser(null);
+	};
 
-  return (
-    <Box>
-      <CartItemsButton />
-      <Tooltip title="Open Settings">
-        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0, ml: 1 }}>
-          <Avatar alt={user.name} src={`${api_base_url}${user.img_url}`} />
-        </IconButton>
-      </Tooltip>
-      <Menu
-        sx={{ mt: "45px" }}
-        id="menu-appbar"
-        anchorEl={anchorElUser}
-        anchorOrigin={{
-          vertical: "top",
-          horizontal: "right",
-        }}
-        keepMounted
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "right",
-        }}
-        open={Boolean(anchorElUser)}
-        onClose={handleCloseUserMenu}
-      >
-        <MenuNavItemLink to="/customer" title="profile" />
-        <MenuNavItemLink to="/customer/wishlist" title="my wishlist" />
-        <MenuNavItemLink to="/customer/orders" title="orders" />
-        <Divider />
-        <Box
-          component={Link}
-          to="/login"
-          onClick={handleLogout}
-          sx={{ textDecoration: "none", color: "inherit" }}
-        >
-          <MenuItem>
-            <Typography variant="button">Log-out</Typography>
-          </MenuItem>
-        </Box>
-      </Menu>
-    </Box>
-  );
+	return (
+		<Box>
+			<CartItemsButton />
+			<Tooltip title="Open Settings">
+				<IconButton onClick={handleOpenUserMenu} sx={{ p: 0, ml: 1 }}>
+					<Avatar alt={user.name} src={`${api_base_url}${user.img_url}`} />
+				</IconButton>
+			</Tooltip>
+			<Menu
+				sx={{ mt: "45px" }}
+				id="menu-appbar"
+				anchorEl={anchorElUser}
+				anchorOrigin={{
+					vertical: "top",
+					horizontal: "right",
+				}}
+				keepMounted
+				transformOrigin={{
+					vertical: "top",
+					horizontal: "right",
+				}}
+				open={Boolean(anchorElUser)}
+				onClose={handleCloseUserMenu}
+			>
+				<MenuNavItemLink to="/customer" title="profile" />
+				<MenuNavItemLink to="/customer/wishlist" title="my wishlist" />
+				<MenuNavItemLink to="/customer/orders" title="orders" />
+				<Divider />
+				<Box
+					component={Link}
+					to="/login"
+					onClick={handleLogout}
+					sx={{ textDecoration: "none", color: "inherit" }}
+				>
+					<MenuItem>
+						<Typography variant="button">Log-out</Typography>
+					</MenuItem>
+				</Box>
+			</Menu>
+		</Box>
+	);
 };
 
 function ClientNavbar({ children }) {
-  const trigger = useScrollTrigger({
-    disableHysteresis: true,
-    threshold: 0,
-  });
-  const [anchorElNav, setAnchorElNav] = useState(null);
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+	const trigger = useScrollTrigger({
+		disableHysteresis: true,
+		threshold: 0,
+	});
+	const [anchorElNav, setAnchorElNav] = useState(null);
+	const handleOpenNavMenu = (event) => {
+		setAnchorElNav(event.currentTarget);
+	};
+	const handleCloseNavMenu = () => {
+		setAnchorElNav(null);
+	};
 
-  const user = useSelector(userSelector);
+	const user = useSelector(userSelector);
 
-  return (
-    <Box>
-      <AppBar
-        elevation={trigger ? 4 : 0}
-        color={trigger ? "primary" : "default"}
-      >
-        <Container>
-          <Toolbar disableGutters>
-            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-              <IconButton
-                size="large"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left",
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: "block", md: "none" },
-                }}
-              >
-                <MenuNavItemLink
-                  to="/"
-                  title="home"
-                  handleCloseNavMenu={handleCloseNavMenu}
-                />
-                <MenuNavItemLink
-                  to="/about-us"
-                  title="About Us"
-                  handleCloseNavMenu={handleCloseNavMenu}
-                />
-                <MenuNavItemLink
-                  to="/products"
-                  title="products"
-                  handleCloseNavMenu={handleCloseNavMenu}
-                />
-              </Menu>
-            </Box>
-            <Box sx={{ flexGrow: { xs: 1, md: 0 } }}>
-              <Box
-                component="img"
-                src="/images/LOGO.png"
-                sx={{ height: 100 }}
-              />
-            </Box>
-            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-              <NavItemLink to="/" title="Home" />
-              <NavItemLink to="/about-us" title="About Us" />
-              <NavItemLink to="/products" title="Products" />
-            </Box>
-            {user.id ? (
-              <UserAvatarMenu />
-            ) : (
-              <Box sx={{ display: "flex" }}>
-                <NavItemLink to="/login" title="Log in" />
-                <NavItemLink to="/sign-up" title="Sign Up" />
-              </Box>
-            )}
-          </Toolbar>
-        </Container>
-      </AppBar>
-      <Box mt={13} minHeight="70vh" mb={6}>
-        <Container>{children}</Container>
-      </Box>
-      <Footer />
-    </Box>
-  );
+	return (
+		<Box>
+			<AppBar
+				elevation={trigger ? 4 : 0}
+				color={trigger ? "primary" : "default"}
+			>
+				<Container>
+					<Toolbar disableGutters>
+						<Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+							<IconButton
+								size="large"
+								onClick={handleOpenNavMenu}
+								color="inherit"
+							>
+								<MenuIcon />
+							</IconButton>
+							<Menu
+								anchorEl={anchorElNav}
+								anchorOrigin={{
+									vertical: "bottom",
+									horizontal: "left",
+								}}
+								keepMounted
+								transformOrigin={{
+									vertical: "top",
+									horizontal: "left",
+								}}
+								open={Boolean(anchorElNav)}
+								onClose={handleCloseNavMenu}
+								sx={{
+									display: { xs: "block", md: "none" },
+								}}
+							>
+								<MenuNavItemLink
+									to="/"
+									title="home"
+									handleCloseNavMenu={handleCloseNavMenu}
+								/>
+								<MenuNavItemLink
+									to="/about-us"
+									title="About Us"
+									handleCloseNavMenu={handleCloseNavMenu}
+								/>
+								<MenuNavItemLink
+									to="/products"
+									title="products"
+									handleCloseNavMenu={handleCloseNavMenu}
+								/>
+							</Menu>
+						</Box>
+						<Box sx={{ flexGrow: { xs: 1, md: 0 } }}>
+							<Box
+								component="img"
+								src="/images/LOGO.png"
+								sx={{ height: 100 }}
+							/>
+						</Box>
+						<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+							<NavItemLink to="/" title="Home" />
+							<NavItemLink to="/about-us" title="About Us" />
+							<NavItemLink to="/products" title="Products" />
+						</Box>
+						{user.id ? (
+							<UserAvatarMenu />
+						) : (
+							<Box sx={{ display: "flex" }}>
+								<NavItemLink to="/login" title="Log in" />
+								<NavItemLink to="/sign-up" title="Sign Up" />
+							</Box>
+						)}
+					</Toolbar>
+				</Container>
+			</AppBar>
+			<Box mt={13} minHeight="70vh" mb={6}>
+				<Container>{children}</Container>
+			</Box>
+			<Footer />
+		</Box>
+	);
 }
 
 export default ClientNavbar;
--- client/src/routes/client/ProductShowcase.jsx
+++ client/src/routes/client/ProductShowcase.jsx
@@ -5,14 +5,14 @@ import { PHPPrice, api_base_url } from "../../app/utils";
 import { enqueueSnackbar } from "notistack";
 
 import {
-  Box,
-  Card,
-  CardMedia,
-  Stack,
-  IconButton,
-  Typography,
-  Button,
-  Rating,
+	Box,
+	Card,
+	CardMedia,
+	Stack,
+	IconButton,
+	Typography,
+	Button,
+	Rating,
 } from "@mui/material";
 
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
@@ -29,231 +29,231 @@ import YardIcon from "@mui/icons-material/Yard";
 import PetsOutlinedIcon from "@mui/icons-material/PetsOutlined";
 import PetsIcon from "@mui/icons-material/Pets";
 import {
-  useGetProductQuery,
-  useViewProductMutation,
+	useGetProductQuery,
+	useViewProductMutation,
 } from "../../app/services/product";
 import LoadingProgress from "../../components/LoadingProgress";
 
 import { userSelector } from "../../features/userSlice";
 import {
-  useAddToCartMutation,
-  useGetCartByUserQuery,
+	useAddToCartMutation,
+	useGetCartByUserQuery,
 } from "../../app/services/cart-item";
 
 const SpecInfo = ({ title, value }) => (
-  <Stack direction="row" spacing={2} justifyContent="center">
-    <Typography textAlign="right" sx={{ width: { xs: 300, md: 500 } }}>
-      {title}:
-    </Typography>
-    <Typography sx={{ width: 500 }}>{value}</Typography>
-  </Stack>
+	<Stack direction="row" spacing={2} justifyContent="center">
+		<Typography textAlign="right" sx={{ width: { xs: 300, md: 500 } }}>
+			{title}:
+		</Typography>
+		<Typography sx={{ width: 500 }}>{value}</Typography>
+	</Stack>
 );
 
 const RatingInfo = ({ title, value, Icon, EmptyIcon }) => (
-  <Stack
-    direction="row"
-    spacing={2}
-    justifyContent="center"
-    alignItems="center"
-  >
-    <Typography textAlign="right" sx={{ width: { xs: 300, md: 500 } }}>
-      {title}:
-    </Typography>
-    <Rating
-      value={value}
-      sx={{ width: 500 }}
-      icon={<Icon fontSize="inherit" />}
-      emptyIcon={<EmptyIcon fontSize="inherit" />}
-      size="large"
-      readOnly
-    />
-  </Stack>
+	<Stack
+		direction="row"
+		spacing={2}
+		justifyContent="center"
+		alignItems="center"
+	>
+		<Typography textAlign="right" sx={{ width: { xs: 300, md: 500 } }}>
+			{title}:
+		</Typography>
+		<Rating
+			value={value}
+			sx={{ width: 500 }}
+			icon={<Icon fontSize="inherit" />}
+			emptyIcon={<EmptyIcon fontSize="inherit" />}
+			size="large"
+			readOnly
+		/>
+	</Stack>
 );
 
 function ProductShowcase() {
-  const { id } = useParams();
-  const {
-    data: product = {},
-    isLoading,
-    isSuccess,
-  } = useGetProductQuery(id);
-  const [isFavorite, setIsFavorite] = useState(false);
+	const { id } = useParams();
+	const {
+		data: product = {},
+		isLoading,
+		isSuccess,
+	} = useGetProductQuery(id);
+	const [isFavorite, setIsFavorite] = useState(false);
 
-  const user = useSelector(userSelector);
-  const [viewProduct] = useViewProductMutation();
-  const [addToCart] = useAddToCartMutation(user.id);
-  const { data: cart_items = [] } = useGetCartByUserQuery(user.id);
+	const user = useSelector(userSelector);
+	const [viewProduct] = useViewProductMutation();
+	const [addToCart] = useAddToCartMutation(user.id);
+	const { data: cart_items = [] } = useGetCartByUserQuery(user.id);
 
-  useEffect(() => {
-    if (isSuccess) {
-      viewProduct({ product: product.name });
-    }
-  }, [isSuccess]);
+	useEffect(() => {
+		if (isSuccess) {
+			viewProduct({ product: product.name });
+		}
+	}, [isSuccess]);
 
-  const handleAddToCart = async () => {
-    if (user.id) {
-      if (!cart_items.some((e) => e.product.id === product.id)) {
-        await addToCart({ customer: user.id, product: product.id })
-          .unwrap()
-          .then((res) => {
-            enqueueSnackbar("Added to cart successfully!", {
-              variant: "success",
-            });
-          });
-      } else {
-        enqueueSnackbar("Product is already in the Cart!", {
-          variant: "warning",
-          preventDuplicate: true,
-        });
-      }
-    } else {
-      enqueueSnackbar("Please Log in First!", {
-        variant: "warning",
-        preventDuplicate: true,
-      });
-    }
-  };
+	const handleAddToCart = async () => {
+		if (user.id) {
+			if (!cart_items.some((e) => e.product.id === product.id)) {
+				await addToCart({ customer: user.id, product: product.id })
+					.unwrap()
+					.then((res) => {
+						enqueueSnackbar("Added to cart successfully!", {
+							variant: "success",
+						});
+					});
+			} else {
+				enqueueSnackbar("Product is already in the Cart!", {
+					variant: "warning",
+					preventDuplicate: true,
+				});
+			}
+		} else {
+			enqueueSnackbar("Please Log in First!", {
+				variant: "warning",
+				preventDuplicate: true,
+			});
+		}
+	};
 
-  let content;
-  if (isLoading) {
-    content = <LoadingProgress />;
-  } else if (isSuccess) {
-    content = (
-      <Box>
-        <Typography variant="h2" textAlign="center" mt={18}>
-          PRODUCT DETAIL
-        </Typography>
-        <Stack
-          direction={{ md: "row" }}
-          spacing={4}
-          justifyContent="center"
-        >
-          <Box sx={{ width: { xs: "100%", md: 400 } }}>
-            <Card
-              sx={{
-                width: { xs: 280, md: 400 },
-                m: { xs: "auto", md: 0 },
-              }}
-              elevation={9}
-            >
-              <CardMedia
-                sx={{
-                  height: { xs: 280, md: 400 },
-                  width: { xs: 280, md: 400 },
-                  backgroundColor: (t) => t.palette.primary.main,
-                }}
-                image={`${api_base_url}${product.img_url}`}
-              >
-                <IconButton
-                  onClick={() => setIsFavorite(!isFavorite)}
-                  sx={{
-                    top: { xs: "80%", md: "85%" },
-                    left: 11,
-                    backgroundColor: "#eee",
-                    boxShadow: "1px 1px 3px",
-                  }}
-                  disableRipple
-                >
-                  {isFavorite ? (
-                    <FavoriteIcon sx={{ color: "red" }} />
-                  ) : (
-                    <FavoriteBorderIcon />
-                  )}
-                </IconButton>
-              </CardMedia>
-            </Card>
-          </Box>
-          <Stack mt={{ xs: 3, md: 0 }}>
-            <Box textAlign={{ xs: "center", md: "left" }}>
-              <Typography variant="h5" fontWeight="bold">
-                {product.name}
-              </Typography>
-              <Typography variant="h6">
-                Category: {product.category}
-              </Typography>
-              <Typography variant="h6">
-                Price: {PHPPrice.format(product.price)}
-              </Typography>
-              <Typography variant="h6">
-                Stocks: {product.stocks}
-              </Typography>
-            </Box>
-            <Stack
-              direction={{ xs: "row", md: "column" }}
-              justifyContent={"center"}
-            >
-              <Button variant="contained" onClick={handleAddToCart}>
-                Add To Cart
-              </Button>
-            </Stack>
-          </Stack>
-        </Stack>
-        <Box mt={3}>
-          <Typography textAlign="center" variant="h5" fontWeight="bold">
-            PRODUCT DESCRIPTION
-          </Typography>
+	let content;
+	if (isLoading) {
+		content = <LoadingProgress />;
+	} else if (isSuccess) {
+		content = (
+			<Box>
+				<Typography variant="h2" textAlign="center" mt={18}>
+					PRODUCT DETAIL
+				</Typography>
+				<Stack
+					direction={{ md: "row" }}
+					spacing={4}
+					justifyContent="center"
+				>
+					<Box sx={{ width: { xs: "100%", md: 400 } }}>
+						<Card
+							sx={{
+								width: { xs: 280, md: 400 },
+								m: { xs: "auto", md: 0 },
+							}}
+							elevation={9}
+						>
+							<CardMedia
+								sx={{
+									height: { xs: 280, md: 400 },
+									width: { xs: 280, md: 400 },
+									backgroundColor: (t) => t.palette.primary.main,
+								}}
+								image={`${api_base_url}${product.img_url}`}
+							>
+								<IconButton
+									onClick={() => setIsFavorite(!isFavorite)}
+									sx={{
+										top: { xs: "80%", md: "85%" },
+										left: 11,
+										backgroundColor: "#eee",
+										boxShadow: "1px 1px 3px",
+									}}
+									disableRipple
+								>
+									{isFavorite ? (
+										<FavoriteIcon sx={{ color: "red" }} />
+									) : (
+										<FavoriteBorderIcon />
+									)}
+								</IconButton>
+							</CardMedia>
+						</Card>
+					</Box>
+					<Stack mt={{ xs: 3, md: 0 }}>
+						<Box textAlign={{ xs: "center", md: "left" }}>
+							<Typography variant="h5" fontWeight="bold">
+								{product.name}
+							</Typography>
+							<Typography variant="h6">
+								Category: {product.category}
+							</Typography>
+							<Typography variant="h6">
+								Price: {PHPPrice.format(product.price)}
+							</Typography>
+							<Typography variant="h6">
+								Stocks: {product.stocks}
+							</Typography>
+						</Box>
+						<Stack
+							direction={{ xs: "row", md: "column" }}
+							justifyContent={"center"}
+						>
+							<Button variant="contained" onClick={handleAddToCart}>
+								Add To Cart
+							</Button>
+						</Stack>
+					</Stack>
+				</Stack>
+				<Box mt={3}>
+					<Typography textAlign="center" variant="h5" fontWeight="bold">
+						PRODUCT DESCRIPTION
+					</Typography>
 
-          <Box>
-            <Stack direction="row" justifyContent="center">
-              <Typography textAlign="right" mr={3} sx={{ width: 300 }}>
-                Description:
-              </Typography>
-              <Typography sx={{ width: 500 }}>
-                {product.description}
-              </Typography>
-            </Stack>
-          </Box>
-        </Box>
-        {product.category == "Plant" && (
-          <Box mt={3}>
-            <Typography textAlign="center" variant="h6" fontWeight="bold">
-              PLANT SPECS
-            </Typography>
-            <Stack spacing={1}>
-              <SpecInfo title="Type" value="Indoor" />
-              <SpecInfo title="Height" value={`${product.height}"`} />
-              <SpecInfo
-                title="Pot Diameter"
-                value={`${product.pot_diameter}"`}
-              />
-              <RatingInfo
-                title="Light Requirements"
-                value={product.light_requirements}
-                Icon={LightModeIcon}
-                EmptyIcon={LightModeOutlinedIcon}
-              />
-              <RatingInfo
-                title="Humidity Needs"
-                value={product.humidity_needs}
-                Icon={LocalDrinkIcon}
-                EmptyIcon={LocalDrinkOutlinedIcon}
-              />
-              <RatingInfo
-                title="Watering Needs"
-                value={product.watering_needs}
-                Icon={WaterDropIcon}
-                EmptyIcon={WaterDropOutlinedIcon}
-              />
-              <RatingInfo
-                title="Repotting"
-                value={product.repotting}
-                Icon={YardIcon}
-                EmptyIcon={YardOutlinedIcon}
-              />
-              <RatingInfo
-                title="Pet Friendly"
-                value={product.pet_friendly}
-                Icon={PetsIcon}
-                EmptyIcon={PetsOutlinedIcon}
-              />
-            </Stack>
-          </Box>
-        )}
-      </Box>
-    );
-  }
+					<Box>
+						<Stack direction="row" justifyContent="center">
+							<Typography textAlign="right" mr={3} sx={{ width: 300 }}>
+								Description:
+							</Typography>
+							<Typography sx={{ width: 500 }}>
+								{product.description}
+							</Typography>
+						</Stack>
+					</Box>
+				</Box>
+				{product.category == "Plant" && (
+					<Box mt={3}>
+						<Typography textAlign="center" variant="h6" fontWeight="bold">
+							PLANT SPECS
+						</Typography>
+						<Stack spacing={1}>
+							<SpecInfo title="Type" value="Indoor" />
+							<SpecInfo title="Height" value={`${product.height}"`} />
+							<SpecInfo
+								title="Pot Diameter"
+								value={`${product.pot_diameter}"`}
+							/>
+							<RatingInfo
+								title="Light Requirements"
+								value={product.light_requirements}
+								Icon={LightModeIcon}
+								EmptyIcon={LightModeOutlinedIcon}
+							/>
+							<RatingInfo
+								title="Humidity Needs"
+								value={product.humidity_needs}
+								Icon={LocalDrinkIcon}
+								EmptyIcon={LocalDrinkOutlinedIcon}
+							/>
+							<RatingInfo
+								title="Watering Needs"
+								value={product.watering_needs}
+								Icon={WaterDropIcon}
+								EmptyIcon={WaterDropOutlinedIcon}
+							/>
+							<RatingInfo
+								title="Repotting"
+								value={product.repotting}
+								Icon={YardIcon}
+								EmptyIcon={YardOutlinedIcon}
+							/>
+							<RatingInfo
+								title="Pet Friendly"
+								value={product.pet_friendly}
+								Icon={PetsIcon}
+								EmptyIcon={PetsOutlinedIcon}
+							/>
+						</Stack>
+					</Box>
+				)}
+			</Box>
+		);
+	}
 
-  return <Box>{content}</Box>;
+	return <Box>{content}</Box>;
 }
 
 export default ProductShowcase;
