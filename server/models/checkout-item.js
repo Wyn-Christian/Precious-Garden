7de453bae2469f6fe8aaf34b95f721fd2af708d7
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2023-07-08 02:36:51 +0800

    Add category in checkout item

--- server/models/checkout-item.js
+++ server/models/checkout-item.js
@@ -2,39 +2,40 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CheckoutItemSchema = new Schema(
-  {
-    product: {
-      type: mongoose.Types.ObjectId,
-      ref: "Product",
-      required: true,
-    },
-    name: String,
-    img_name: String,
-    quantity: {
-      type: Number,
-      required: true,
-    },
-    price: {
-      type: mongoose.Types.Decimal128,
-      get: getValue,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-    toJSON: {
-      getters: true,
-    },
-  }
+	{
+		product: {
+			type: mongoose.Types.ObjectId,
+			ref: "Product",
+			required: true,
+		},
+		name: String,
+		img_name: String,
+		category: { type: String, default: "Plant" },
+		quantity: {
+			type: Number,
+			required: true,
+		},
+		price: {
+			type: mongoose.Types.Decimal128,
+			get: getValue,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+		toJSON: {
+			getters: true,
+		},
+	}
 );
 CheckoutItemSchema.virtual("img_url").get(function () {
-  return `/images/products/${this.img_name}`;
+	return `/images/products/${this.img_name}`;
 });
 function getValue(value) {
-  if (typeof value !== "undefined") {
-    return parseFloat(value.toString());
-  }
-  return value;
+	if (typeof value !== "undefined") {
+		return parseFloat(value.toString());
+	}
+	return value;
 }
 
 module.exports = mongoose.model("Checkout-Item", CheckoutItemSchema);
