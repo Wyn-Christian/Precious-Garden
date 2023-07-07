77a84dac21fa1784e6935cf694b28ca3afcf66a3
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2023-07-08 03:10:57 +0800

    Feature: message when no wishlist

--- client/src/routes/client/customer/WishList.jsx
+++ client/src/routes/client/customer/WishList.jsx
@@ -7,29 +7,34 @@ import { userSelector } from "../../../features/userSlice";
 import { useGetWishlistByUserQuery } from "../../../app/services/wishlist";
 
 function WishList() {
-  const { data: products = [] } = useGetProductsQuery();
-  const user = useSelector(userSelector);
-  const { data: wishlist = [] } = useGetWishlistByUserQuery(user.id);
+	const { data: products = [] } = useGetProductsQuery();
+	const user = useSelector(userSelector);
+	const { data: wishlist = [] } = useGetWishlistByUserQuery(user.id);
 
-  return (
-    <Box>
-      <Typography
-        textAlign="center"
-        variant="h2"
-        fontWeight="bold"
-        mt={18}
-      >
-        WISHLISTS
-      </Typography>
-      <Grid container spacing={3}>
-        {products
-          .filter((p) => wishlist.includes(p.id))
-          .map((p) => (
-            <ProductCard isWishlist key={p.id} {...p} />
-          ))}
-      </Grid>
-    </Box>
-  );
+	return (
+		<Box>
+			<Typography
+				textAlign="center"
+				variant="h2"
+				fontWeight="bold"
+				mt={18}
+			>
+				WISHLISTS
+			</Typography>
+			<Grid container spacing={3}>
+				{products
+					.filter((p) => wishlist.includes(p.id))
+					.map((p) => (
+						<ProductCard isWishlist key={p.id} {...p} />
+					))}
+			</Grid>
+			{!wishlist.length && (
+				<Typography textAlign="center" mt={3}>
+					Add new wishlists in products...
+				</Typography>
+			)}
+		</Box>
+	);
 }
 
 export default WishList;
