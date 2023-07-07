587cd81a726f35c9ef1139622650afce51e93557
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2023-07-08 02:35:12 +0800

    Fix CategoryInfo

--- client/src/components/CategoryInfo.jsx
+++ client/src/components/CategoryInfo.jsx
@@ -1,23 +1,22 @@
 import { Box, Chip } from "@mui/material";
 
 const CategoryInfo = ({ row }) => {
-  let content;
-
-  switch (row.category) {
-    case "Plant":
-      content = <Chip sx={{ backgroundColor: "#C7E9B0" }} label="Plant" />;
-      break;
-    case "Pot":
-      content = <Chip sx={{ backgroundColor: "#BAD1C2" }} label="Pot" />;
-      break;
-    case "Soil":
-      content = <Chip sx={{ backgroundColor: "#FFD495" }} label="Soil" />;
-      break;
-    case "Tool":
-      content = <Chip sx={{ backgroundColor: "#EAE0DA" }} label="Tool" />;
-      break;
-  }
-  return <Box m="auto">{content}</Box>;
+	let content;
+	switch (row.category) {
+		case "Plant":
+			content = <Chip sx={{ backgroundColor: "#C7E9B0" }} label="Plant" />;
+			break;
+		case "Pot":
+			content = <Chip sx={{ backgroundColor: "#BAD1C2" }} label="Pot" />;
+			break;
+		case "Soil":
+			content = <Chip sx={{ backgroundColor: "#FFD495" }} label="Soil" />;
+			break;
+		case "Tool":
+			content = <Chip sx={{ backgroundColor: "#EAE0DA" }} label="Tool" />;
+			break;
+	}
+	return <Box m="auto">{content}</Box>;
 };
 
 export default CategoryInfo;
