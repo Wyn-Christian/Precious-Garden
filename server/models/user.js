68d0bc83840524a75310269138d5b93e1dd73343
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2023-07-12 15:37:08 +0800

    Letsgooo

--- server/models/user.js
+++ server/models/user.js
@@ -2,70 +2,70 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    img_name: String,
-    position: {
-      type: String,
-      enum: ["admin", "customer"],
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		img_name: String,
+		position: {
+			type: String,
+			enum: ["admin", "customer"],
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
 );
 UserSchema.virtual("img_url").get(function () {
-  return `/images/users/${this.img_name}`;
+	return `/images/users/${this.img_name}`;
 });
 const User = mongoose.model("User", UserSchema);
 
 const CustomerSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ["non-verified", "verified", "blocked"],
-    default: "non-verified",
-  },
-  phone: { type: String, default: "n/a" },
-  address: { type: String, default: "n/a" },
-  wishlist: [
-    {
-      type: mongoose.Types.ObjectId,
-      ref: "Product",
-    },
-  ],
+	username: {
+		type: String,
+		required: true,
+	},
+	status: {
+		type: String,
+		enum: ["non-verified", "verified", "blocked"],
+		default: "non-verified",
+	},
+	phone: { type: String, default: "n/a" },
+	address: { type: String, default: "n/a" },
+	wishlist: [
+		{
+			type: mongoose.Types.ObjectId,
+			ref: "Product",
+		},
+	],
 });
 const Customer = User.discriminator("Customer", CustomerSchema);
 
 const LoginCustomerSchema = new Schema(
-  {
-    login: { type: Number, default: 1 },
-    timestamp: { type: Date, default: Date.now },
-  },
-  {
-    timeseries: {
-      timeField: "timestamp",
-    },
-  }
+	{
+		login: { type: Number, default: 1 },
+		timestamp: { type: Date, default: Date.now },
+	},
+	{
+		timeseries: {
+			timeField: "timestamp",
+		},
+	}
 );
 const LoginCustomer = mongoose.model(
-  "login-customer",
-  LoginCustomerSchema
+	"login-customer",
+	LoginCustomerSchema
 );
 
 module.exports = { User, Customer, LoginCustomer };
